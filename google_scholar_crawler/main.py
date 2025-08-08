7b43ee49c5472be122cba495e3b9c22ce9ea1b55
PengyuWang <107847507+PengyvWANG@users.noreply.github.com>
2025-08-08 15:14:37 +0800

    Update main.py

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import os
 
 # author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
-author: dict = scholarly.search_author_id(j4EqUL8AAAAJ)
+author: dict = scholarly.search_author_id('j4EqUL8AAAAJ')
 
 
 scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
